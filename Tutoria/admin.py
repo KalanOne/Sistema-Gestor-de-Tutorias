473686f72bb7734f3205dd24589ccf1412be7256
Ulis3197 <62312455+Ulis3197@users.noreply.github.com>
2022-10-18 20:32:23 -0500

    Add files via upload

---
+++ Tutoria/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+admin.site.register(Estado)
+admin.site.register(Institucion)
+admin.site.register(Grupo)
+admin.site.register(DepartamentoAcademico)
+admin.site.register(PadreMadreTutor)
+admin.site.register(Tutorado)
+admin.site.register(PersonalTec)
+admin.site.register(PersonalMed)
+admin.site.register(Cuestionario)
+admin.site.register(GrupoRespuesta)
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
+admin.site.register(RespuestaContestada)
+admin.site.register(PIT)
+admin.site.register(PAT)
+admin.site.register(ReporteSemestralGrupal)
+admin.site.register(ReporteSemestralDepartamento)
+admin.site.register(ReporteSemestralInstitucional)
+admin.site.register(ConstanciaTutor)
+admin.site.register(Orden)
+admin.site.register(Motivo)
+admin.site.register(Cita)
