f67b9d593a7e001eba64ac5a11b20ceb85958635
Ulis3197 <rojasulises30@gmail.com>
2022-10-20 01:28:56 -0500

    Perfil todos y tutorado muestran info

--- Tutoria/forms.py
+++ Tutoria/forms.py
@@ -35,3 +35,34 @@ class PadreMadreTutorForm(forms.ModelForm):
             'apellidoMaterno': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'}),
             'telefono': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'})
         }
+
+
+class GrupoForm(forms.ModelForm):
+    class Meta:
+        model = Grupo
+        fields = '__all__'
+        widgets = {
+            'grupo': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'})
+        }
+
+
+class DepartamentoAcademicoForm(forms.ModelForm):
+    class Meta:
+        model = DepartamentoAcademico
+        fields = '__all__'
+        widgets = {
+            'departamentoAcademico': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'}),
+            'abreviacion': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'})
+        }
+
+
+class PerfilPersonalTecForm(forms.ModelForm):
+    class Meta:
+        model = PersonalTec
+        fields = '__all__'
+        widgets = {
+            'domicilio': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'}),
+            'telefono': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'}),
+            'correoPersonal': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'}),
+            'edificio': forms.TextInput(attrs={'class':'form-control', 'disabled':'true'})
+        }
